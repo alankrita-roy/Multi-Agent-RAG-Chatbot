be8cc5f5ff4c880744ddd7f6e441af784df9ad12
alankrita-roy <138924509+alankrita-roy@users.noreply.github.com>
2025-07-13 01:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ llm = ChatGroq(groq_api_key=groq_api_key, model_name="Gemma2-9b-It")
 
 # Tool Wrappers
 arxiv = ArxivQueryRun(api_wrapper=ArxivAPIWrapper(top_k_results=1))
-serp = SerpAPIWrapper(serpapi_api_key=serp_api_key, engine="google")
+serp = SerpAPIWrapper(serpapi_api_key=serp_api_key)
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(top_k_results=1))
 
 # State
